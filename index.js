c03ed260e7c96c3a489d2c1658d53a3e12d230d6
katychillingworth <katy.chillingworth@hotmail.com>
2023-01-06 19:50:32 +0000

    final changes

--- index.js
+++ index.js
@@ -136,4 +136,19 @@ console.log("Greatest Increase in Profits: " + greatestIncreaseMonth + " ($" + g
 var greatestDecrease = 0;
 var greatestDecreaseMonth = '';
 
+for (var i = 0; i < finances.length; i++) {
+
+    if (i > 0) {
+      var change = finances[i][1] - finances[i - 1][1];
+  
+      if (change < greatestDecrease) {
+        greatestDecrease = change;
+        greatestDecreaseMonth = finances[i][0];
+  
+      }
+    }
+  }
+
+  console.log("Greatest Decrease in Profits: " + greatestDecreaseMonth + " ($" + greatestDecrease + ")")
+
 console.log("-----------------------")
